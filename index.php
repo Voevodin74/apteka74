3ab81bf4c3294c65552ee800423a93224de014c0
Андрей Воеводин <74voevodin@gmail.com>
2014-12-14 11:03:56 +0600

    Update index.php
    Закрываем сессию

--- index.php
+++ index.php
@@ -27,3 +27,11 @@ $ses_id = session_id();
 			}
 ?>
 место для футера
+<?
+$end_time = microtime(); 
+$end_array = explode(" ",$end_time); 
+$end_time = $end_array[1] + $end_array[0]; 
+// вычитаем из конечного времени начальное 
+$time = $end_time - $start_time; 
+
+?>
