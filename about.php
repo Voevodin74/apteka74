8d37362acd1ac02bbe2df151a72ebfefca714779
Александра Шашкова <Cuuiii@rambler.ru>
2014-12-15 03:30:52 +0500

    Update about.php

--- about.php
+++ about.php
@@ -42,3 +42,22 @@ if(mysql_errno()==0)
 {
 while($massive = mysql_fetch_array($zapros))
 {
+echo "
+<div class='product left position-relative margin-right-1'>
+<p class='no-margin position-absolute bottom-20 left-20'>
+<a href=http://apteka-74.ru/buy/".htmlspecialchars($massive['id']).">".htmlspecialchars($massive['med_name'])." <img src=http://apteka-74.ru/img_files/kor.png width=15 height=15><br>".htmlspecialchars($massive['med_price'])." руб </p> 
+<img src=".htmlspecialchars($massive['med_img'])." width=230 height=150> 
+</a>
+</div>
+"; 
+$counting = $massive['count(id)']; 
+}
+}
+else { echo 'Не работает <!— '.mysql_errno().": ". mysql_error() .' —>'; }
+$text_zaprosa = 'select count(id) from med';
+$zapros = mysql_query($text_zaprosa);
+$massive = mysql_fetch_array($zapros);
+$total = $massive['count(id)'];
+?> 
+</div>
+</div>
