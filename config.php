488bdc3edc3238bab6b829ec1b335c88fe577da0
Андрей Воеводин <74voevodin@gmail.com>
2014-12-14 11:06:46 +0600

    Update config.php
    Подключаем наш проект к базе данных

--- config.php
+++ config.php
@@ -1,1 +1,6 @@
-гп
+<?
+$dbhost     = "voevodin.mysql"; // Хост или IP-адрес бд mysql
+$dbname     = "voevodin_mysql";      // Имя пользователя mysql
+$dbpassword = "11111"; Пароль пользователя mysql
+$current_db = "voevodin_bd";    // Имя используемой базы данных
+?>
