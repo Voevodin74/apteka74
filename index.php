066d6935508db75e6f7b80a705aecfc19106fc16
Андрей Воеводин <74voevodin@gmail.com>
2014-12-14 11:00:49 +0600

    Update index.php
    Ограничиваем время работы сессии

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?
 include("config.php"); 
  session_start();
+ 
+ // считываем текущее время 
+$start_time = microtime(); 
+// разделяем секунды и миллисекунды (становятся значениями начальных ключей массива-списка) 
+$start_array = explode(" ",$start_time); 
+//стартовое время 
+$start_time = $start_array[1] + $start_array[0];
+
+$ses_id = session_id(); 
+
 
 ?>
