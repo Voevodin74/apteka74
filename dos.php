58d3da9859dd4dfe41b5a95f9b0c413bace96100
Андрей Воеводин <74voevodin@gmail.com>
2014-12-15 19:33:06 +0600

    Create dos.php

---
+++ dos.php
@@ -0,0 +1,11 @@
+<div id="grid-2" class="left full background-offset-single">
+                        <div id="grid-2-inner" class="left full">
+                           <div class="column-2 left">
+                               <div class="column-2-inner left-inner padding-20">
+           
+<p><b>Федеральный закон о лекарственных средствах</b></p>
+Вы можете ознакомиться с правовыми вопросами по следующим ссылкам:</br>
+<p><a href="http://www.fptl.ru/normativi/fz-122.html">Федеральный закон о лекарственных средствах</a></p>
+<p><a href="http://www.rateshops.ru/drugstore_medicine.php">Возврат лекарственных средств</a></p>
+Мы не работает с клиентами младше 18 лет. Мы не продаем товары, требующие рецепта. Наличие товара можно отслеживать на сайте.
+ </div>
