a4f67dc9ef736371faac3f093a2ef4b7d9ea4d3e
Андрей Воеводин <74voevodin@gmail.com>
2014-12-15 19:30:58 +0600

    Update kor_del.php

--- kor_del.php
+++ kor_del.php
@@ -7,4 +7,9 @@ if($_GET['do']=='approve'&&$nomer>0) {
 $text_zaprosa = "UPDATE `korzina` SET `korzina_session`='FAILURE' where `korzina_session`='$ses_id' and `korzina_id`=$nomer";
 $zapros = mysql_query($text_zaprosa);
 
+print "<div class=\"alert\"><b>Товар удален из корзины!</b>
+<p /><a href=http://apteka-74.ru/index.php>Вы будете автоматически перенаправленны на главную страницу</a></div>";
+
+}
+
 ?>
