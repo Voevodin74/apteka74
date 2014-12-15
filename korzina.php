23854e2b976836f4a3b5e0c29459952ed6d60cef
Андрей Воеводин <74voevodin@gmail.com>
2014-12-15 19:18:26 +0600

    Update korzina.php
    Формируем запрос к бд

--- korzina.php
+++ korzina.php
@@ -1,2 +1,15 @@
 <?
+error_reporting(0);
+//больше 5 покупок пользователь не увидит
+$over = 5;	
+//массив в котором перетягиваем идентификаторы покупок
+$myarray = array();
+
+if(empty($start)) { $start = 0; }//Это условие нам необходимо для начальной страницы
+//выводим список наших покупок,если совпадает идентификатор сессии
+
+$text_zaprosa = "select * from korzina where korzina_session='".$ses_id."'";
+$zapros = mysql_query($text_zaprosa);
+if(mysql_errno()==0)
+
 ?>
