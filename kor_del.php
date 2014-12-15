b96808c1070c6e6f5d3c4010bedbc0a213f1d743
Андрей Воеводин <74voevodin@gmail.com>
2014-12-15 19:30:29 +0600

    Update kor_del.php
    Формируем запрос к базе данных

--- kor_del.php
+++ kor_del.php
@@ -2,5 +2,9 @@
 include("functions.php"); 
 session_start();
 $ses_id = session_id(); 
+$nomer = mysql_real_escape_string($_GET['nomer']);
+if($_GET['do']=='approve'&&$nomer>0) {
+$text_zaprosa = "UPDATE `korzina` SET `korzina_session`='FAILURE' where `korzina_session`='$ses_id' and `korzina_id`=$nomer";
+$zapros = mysql_query($text_zaprosa);
 
 ?>
